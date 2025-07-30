c57dfa477fdedc39851e05ade38bcf1bcfab77a1
PORTATIL_MARCO\marco <marco.casati74@gmail.com>
2025-07-30 18:14:08 +0200

    creo fichero shell

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
 import "./style.css";
 
-import {} from "./motor";
-
-import { eventos, inicioPartida } from "./ui";
-
-document.addEventListener("DOMContentLoaded", () => {
-  inicioPartida();
-  eventos();
-});
+import "./shell";
